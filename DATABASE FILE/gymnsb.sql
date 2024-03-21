4237e32e0b3387a0fea8a59b496d79477ee4deae
Ilyesbouzayen <ilyesbouzayen@hotmail.com>
2024-03-21 17:03:57 +0100

    First Commit

---
+++ DATABASE FILE/gymnsb.sql
@@ -0,0 +1,356 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 02, 2022 at 06:18 PM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `gymnsb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`user_id`, `username`, `password`, `name`) VALUES
+(2, 'admin', 'd52bbe7ed46b1f2182072ac3c98fd7be', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `announcements`
+--
+
+CREATE TABLE `announcements` (
+  `id` int(11) NOT NULL,
+  `message` varchar(100) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `announcements`
+--
+
+INSERT INTO `announcements` (`id`, `message`, `date`) VALUES
+(7, 'This is to announce that our GYM will remain close for 51 days due to COVID-19.', '2020-03-30'),
+(8, 'Opening of GYM Halls and Clubs are not fixed yet. Stay tuned for more updates!!', '2020-04-03'),
+(9, 'Renovation Going On...', '2020-04-04'),
+(10, 'This is a demo announcement from admin', '2022-06-03');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attendance`
+--
+
+CREATE TABLE `attendance` (
+  `id` int(11) NOT NULL,
+  `user_id` varchar(100) NOT NULL,
+  `curr_date` text NOT NULL,
+  `curr_time` text NOT NULL,
+  `present` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `equipment`
+--
+
+CREATE TABLE `equipment` (
+  `id` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `amount` int(100) NOT NULL,
+  `quantity` int(100) NOT NULL,
+  `vendor` varchar(50) NOT NULL,
+  `description` varchar(50) NOT NULL,
+  `address` varchar(20) NOT NULL,
+  `contact` varchar(10) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `equipment`
+--
+
+INSERT INTO `equipment` (`id`, `name`, `amount`, `quantity`, `vendor`, `description`, `address`, `contact`, `date`) VALUES
+(3, 'Treadmill', 909, 4, 'DnS', 'Edited Description', '7 Cedarstone Drive', '8521479633', '2019-03-07'),
+(4, 'Vertical Press Machine', 949, 3, 'SS Industries', 'For Biceps And Triceps, Upper Back, Chest', '7 Cedarstone Drive', '1245558980', '2020-03-19'),
+(5, 'Dumbell - Adjustable', 102, 26, 'Uptown Suppliers', 'Material: Steel, Rubber Plastic, Concrete', '7 Cedarstone Drive', '9875552100', '2020-03-29'),
+(6, 'Multi Bench Press Machine', 219, 2, 'DnS Suppliers', '6 In 1 Multi Bench With Incline, Flat, Decline Ben', '7 Cedarstone Drive', '7410001010', '2020-04-05'),
+(7, 'Demo', 265, 5, 'Demo', 'This is a demo test.', '77 Demo Lane', '8524445452', '2020-04-03'),
+(10, 'RowWarrior Fitness Rowing Mach', 5616, 12, 'Roww Stores', 'HIGHEST QUALITY: This best of class air rowing mac', '52 Weekley Street', '7412585555', '2021-06-12');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `members`
+--
+
+CREATE TABLE `members` (
+  `user_id` int(11) NOT NULL,
+  `fullname` varchar(20) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `gender` varchar(20) NOT NULL,
+  `dor` date NOT NULL,
+  `services` varchar(50) NOT NULL,
+  `amount` int(100) NOT NULL,
+  `paid_date` date NOT NULL,
+  `p_year` int(11) NOT NULL,
+  `plan` varchar(100) NOT NULL,
+  `address` varchar(20) NOT NULL,
+  `contact` varchar(10) NOT NULL,
+  `status` varchar(20) NOT NULL DEFAULT 'Active',
+  `attendance_count` int(100) NOT NULL,
+  `ini_weight` int(100) NOT NULL DEFAULT '0',
+  `curr_weight` int(100) NOT NULL DEFAULT '0',
+  `ini_bodytype` varchar(50) NOT NULL,
+  `curr_bodytype` varchar(50) NOT NULL,
+  `progress_date` date NOT NULL,
+  `reminder` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `members`
+--
+
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rates`
+--
+
+CREATE TABLE `rates` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `charge` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `rates`
+--
+
+INSERT INTO `rates` (`id`, `name`, `charge`) VALUES
+(1, 'Fitness', '55'),
+(2, 'Sauna', '35'),
+(3, 'Cardio', '40');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reminder`
+--
+
+CREATE TABLE `reminder` (
+  `id` int(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `message` text NOT NULL,
+  `status` text NOT NULL,
+  `date` datetime NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `reminder`
+--
+
+INSERT INTO `reminder` (`id`, `name`, `message`, `status`, `date`, `user_id`) VALUES
+(12, 'staff', 'asd', 'unread', '2020-04-16 22:39:59', 0),
+(13, 'staff', 'asdasdas', 'unread', '2020-04-16 22:40:49', 0),
+(14, 'staff', 'ASasA', 'unread', '2020-04-16 22:41:59', 0),
+(15, 'staff', 'asdasdasd', 'unread', '2020-04-16 22:42:28', 0);
+
+INSERT INTO `members` (`user_id`, `fullname`, `username`, `password`, `gender`, `dor`, `services`, `amount`, `paid_date`, `p_year`, `plan`, `address`, `contact`, `status`, `attendance_count`, `ini_weight`, `curr_weight`, `ini_bodytype`, `curr_bodytype`, `progress_date`, `reminder`) VALUES
+(31, 'darshan a r', 'darshanraj24@gmail.c', '406f84c0877f9574a5295bb8f4d0ee6f', 'Male', '2022-12-17', 'Cardio', 0, '0000-00-00', 0, '6', 'hassan', '8496967313', 'Pending', 0, 0, 0, '', '', '0000-00-00', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `staffs`
+--
+
+CREATE TABLE `staffs` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `fullname` varchar(50) NOT NULL,
+  `address` varchar(20) NOT NULL,
+  `designation` varchar(20) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `contact` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `staffs`
+--
+
+INSERT INTO `staffs` (`user_id`, `username`, `password`, `email`, `fullname`, `address`, `designation`, `gender`, `contact`) VALUES
+(1, 'likhi', '772f5b448d860b86356f90bb65db65f6', 'likhithvklikhi@gmail.com', 'Likhith v k', 'Hassan', 'Cashier', 'Male', 9380144961),
+(2, 'nitin', '321187022244b2e2ef92eb573fa11178', 'nitink@gmail.com', 'Nitin K Bhardwaj', 'Hassan', 'Trainer', 'Female', 2147483647),
+(3, 'manoj', '977c0156d7403e2bebba75cdf5652678', 'jamesb@mail.com', 'Manoj kulal', 'Hassan', 'Trainer', 'Male', 2147483647);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `todo`
+--
+
+CREATE TABLE `todo` (
+  `id` int(11) NOT NULL,
+  `task_status` varchar(50) NOT NULL,
+  `task_desc` varchar(30) NOT NULL,
+  `user_id` int(7) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `todo`
+--
+
+INSERT INTO `todo` (`id`, `task_status`, `task_desc`, `user_id`) VALUES
+(20, 'In Progress', 'Test Completed', 14),
+(21, 'Pending', 'Mastering Crunches', 6),
+(22, 'In Progress', 'Standing Workouts For Flat Abs', 6),
+(23, 'In Progress', 'Triceps Buildup - 3 set', 14),
+(24, 'Pending', 'Decline dumbbell bench press', 6),
+(27, 'Pending', 'dddd', 0),
+(28, 'In Progress', 'Test 1', 23);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `announcements`
+--
+ALTER TABLE `announcements`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `attendance`
+--
+ALTER TABLE `attendance`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `equipment`
+--
+ALTER TABLE `equipment`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `members`
+--
+ALTER TABLE `members`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `rates`
+--
+ALTER TABLE `rates`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `reminder`
+--
+ALTER TABLE `reminder`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `staffs`
+--
+ALTER TABLE `staffs`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `todo`
+--
+ALTER TABLE `todo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `announcements`
+--
+ALTER TABLE `announcements`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+--
+-- AUTO_INCREMENT for table `attendance`
+--
+ALTER TABLE `attendance`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+--
+-- AUTO_INCREMENT for table `equipment`
+--
+ALTER TABLE `equipment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+--
+-- AUTO_INCREMENT for table `members`
+--
+ALTER TABLE `members`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+--
+-- AUTO_INCREMENT for table `rates`
+--
+ALTER TABLE `rates`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `reminder`
+--
+ALTER TABLE `reminder`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+--
+-- AUTO_INCREMENT for table `staffs`
+--
+ALTER TABLE `staffs`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `todo`
+--
+ALTER TABLE `todo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ README.md
@@ -0,0 +1,2 @@
+# full-stack-gym
+Built a Fully responsive ,full stack website to reduce manual work of customers as per the request raise by the client. Includes devoping VR of gym ,google fit API,Web application that is maintained by admin and payment gateway . Tech Stacks: HTML,CSS,JS,SQL,PHP
